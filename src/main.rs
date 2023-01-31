980afbc9863e80498370c13960ee15987266523b
Brian Stearns <brianw.stearns@gmail.com>
2023-01-31 14:33:25 -0500

    Circadian avg now works

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 
 fn circadian_average(range: f64, data: Vec<f64>) -> (f64, f64) {
+    let num_samples = data.len();
     // Get X, Y position of each data point on a circle with a perimeter of range
     let positions: Vec<(f64, f64)> = data
         .iter()
@@ -12,23 +13,20 @@ fn circadian_average(range: f64, data: Vec<f64>) -> (f64, f64) {
         })
         .collect();
     println!("Positions: {:?}", positions);
-    // let avg_x_pos = positions.map(|(x, _)| x).sum::<f64>() / data.len() as f64;
-    // let avg_y_pos = positions.map(|(_, y)| y).sum::<f64>() / data.len() as f64;
-    let (avg_x_pos, avg_y_pos) = positions
-        .iter()
-        .fold((0.0, 0.0), |(x, y), (x2, y2)| (x + x2, y + y2));
-    let avg_pos = (avg_x_pos, avg_y_pos);
+    let avg_x_pos = positions.iter().map(|(x, _)| x).sum::<f64>() / data.len() as f64;
+    let avg_y_pos = positions.iter().map(|(_, y)| y).sum::<f64>() / data.len() as f64;
     // Get the angle of the average position
-    let avg_angle = avg_pos.1.atan2(avg_pos.0);
+    let avg_angle = avg_y_pos.atan2(avg_x_pos);
     // Convert the angle to a value on the range
     let avg_value = (avg_angle / (2.0 * std::f64::consts::PI)) * range;
-    // Get the error, which is the distance from the average position to the origin
-    let error = (avg_pos.0.powi(2) + avg_pos.1.powi(2)).sqrt();
-    (avg_value, error)
+    // Get the confidence, which is the distance from the origin
+    println!("Average position: {:?}", (avg_x_pos, avg_y_pos));
+    let confidence = (avg_x_pos.powi(2) + avg_y_pos.powi(2)).sqrt() as f64;
+    (avg_value, confidence)
 }
 
 fn main() {
-    let data = vec![0.0]; //, 1.0];
-    let (avg, error) = circadian_average(4.0, data);
-    println!("Average: {}, Error: {}", avg, error);
+    let data = vec![0.0, 0.0];
+    let (avg, confidence) = circadian_average(4.0, data);
+    println!("Average: {}, Error: {}", avg, confidence);
 }
